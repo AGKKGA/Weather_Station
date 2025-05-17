585b390475910940baaca576567ca9304728d471
AGKKGA <abdulghanikhan2000@gmail.com>
2025-05-17 17:48:42 +0300

    Changed the USB port number

--- tasks.py
+++ tasks.py
@@ -6,16 +6,17 @@ def dev(ctx):
     """Run both the data collection script and the Flask app."""
     try:
         # Run the data collection script in the background
-        data_process = subprocess.Popen(["python", "./Weather-Station/MQ2andDHT11.py"])
+        data_process = subprocess.Popen(["python", "scripts/MQ2andDHT11.py"])
         print("Data collection started...")
 
         # Run the Flask app
-        flask_process = subprocess.Popen(["python", "./Weather-Station/weather_app/app.py"])
+        flask_process = subprocess.Popen(["python", "weather_app/app.py"])
         print("Flask app started at http://0.0.0.0:5000")
 
         # Keep both processes running
         data_process.wait()
         flask_process.wait()
+
     except KeyboardInterrupt:
         print("Shutting down...")
         data_process.terminate()
